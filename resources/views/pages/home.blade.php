199f988f164aeba59019863b6adb423edebde1fe
TullioTambone <tulliotambone97@gmail.com>
2023-06-09 13:59:53 +0200

    set up installazioni + creazione db + layout base

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,11 @@
+@extends( 'layout.app' )
+
+@section('titlePage')
+    Laravel App | Home
+@endsection
+
+@section( 'mainContent' )
+
+<h1>hello main</h1>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $title = 'Pagina Home';
+
+    return view('pages/home', compact('title'));
+})->name('home');
