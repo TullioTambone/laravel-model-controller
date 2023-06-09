32159d350af976f139be175fbc8440538168e2bb
TullioTambone <tulliotambone97@gmail.com>
2023-06-09 16:10:59 +0200

    aggiusto file php.ini - chiamata in pagina elementi

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,7 +6,20 @@
 
 @section( 'mainContent' )
 
-@foreach( $movies as $element)
-    <h4>{{ $element['title'] }}</h4>
-@endforeach
+<div class="contianer">
+    <div class="row justify-content-center">
+        @foreach( $movies as $element)
+
+        <div class="col-12 col-md-4 col-lg-4 card">
+            <h4>{{ $element['title'] }}</h4>
+            <h6>original title: {{ $element['original_title'] }}</h6>
+            <span class="d-block">{{ $element['nationality'] }}</span>
+            <span class="d-block">{{ $element['date'] }}</span>
+            <span class="d-block">voto: {{ $element['vote'] }}</span>
+        </div>
+        @endforeach
+    </div>
+</div>
+
+
 @endsection
